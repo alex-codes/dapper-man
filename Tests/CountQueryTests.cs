6d933ee0ddcdfac3b6672a838af21f71fc08d0cd
Alex <alex-codes@users.noreply.github.com>
2018-06-15 19:56:15 -0500

    added unit test project and unit tests for count query and property cache

---
+++ Tests/CountQueryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DapperMan.Core;
+using DapperMan.MsSql;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CountQueryTests
+    {
+        [TestMethod]
+        public void CountQuery_GenerateStatement()
+        {
+            string expected = @"
+SELECT [Count] = COUNT(*)
+FROM dbo.TableName
+WHERE A = @a
+AND B = @b;";
+
+            expected = expected
+                .Replace(Environment.NewLine, " ")
+                .Trim();
+
+            var query = DapperQuery.Count("dbo.TableName", "constr")
+                .Where("A = @a")
+                .Where("B = @b");
+
+            Assert.AreEqual(expected, ((IQueryGenerator)query).GenerateStatement());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CountQuery_Where_ThrowsWhenNull()
+        {
+            DapperQuery.Count("test", "test")
+                .Where(null);
+        }
+    }
+}
---
+++ Tests/PropertyCacheTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Caching;
+using DapperMan.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class PropertyCacheTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PropertyCache_Ctor_Throws_When_Cache_Null()
+        {
+            var cache = new PropertyCache(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PropertyCache_Ctor_Throws_When_Policy_Null()
+        {
+            var cache = new PropertyCache(new MemoryCache("test"), null);
+        }
+
+        [TestMethod]
+        public void PropertyCache_Ctor_Uses_Default_Policy_When_Null()
+        {
+            var cache = new PropertyCache(new MemoryCache("test"));
+            Assert.AreEqual(cache.Policy.SlidingExpiration, PropertyCache.DefaultCachePolicy.SlidingExpiration);
+        }
+    }
+}
